90435f5e09f551bfcc74410a0f47bd2d9400e937
John Gallo <Johnpgallo1@gmail.com>
2017-08-01 15:07:37 -0400

    Clock
    Counting down

---
+++ clock.py
@@ -0,0 +1,18 @@
+import clock, sys, pygame
+
+'' define player one
+Class Player1:
+  def __init__(self,time,lives,position):
+  self.t = p1time
+  self.l = p1lives
+  self.p = p1position
+
+Player1 = P1
+P1 = (60,3,defense)
+
+
+offense, defense = defense, offense
+
+
+  
+  
