cac5e244b127afc2ff146835e70a58269528ad01
John Gallo <Johnpgallo1@gmail.com>
2017-08-01 16:37:39 -0400

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,18 +1,37 @@
 import clock, sys, pygame
+from random import randint
 
-'' define player one
+#initializing pygame display and setting size
+pygame.display.init()
+screen_width = 800
+screen_height = 800
+screen = pygame.display.set_mode([screen_width, screen_height])
+
+# define First Players time, lives, and starting position
 Class Player1:
-  def __init__(self,time,lives,position):
-  self.t = p1time
-  self.l = p1lives
-  self.p = p1position
+  def __init__(self, time, lives, position):
+  self.t = time
+  self.l = lives
+  self.p = position
 
 Player1 = P1
 P1 = (60,3,defense)
 
+Class Player2:
+   def __init__(self, time, lives, position):
+      self.t = time
+      self.l = lives
+      self.p = position
+Player2 = P2
+P2 = (60,3,offense)
+
 
 offense, defense = defense, offense
 
+# Event randomly selecting who goes first
 
-  
-  
+for event in pygame.event.get():
+  if event.type == pygame.QUIT:
+    sys.exit()
+
+# Event starting the game after Offensive player decided
